98083318babacf8138db5c9610322b4fe859e35b
Dr. Pan Huang <59990648+Baron-Huang@users.noreply.github.com>
2025-11-27 01:09:53 +0800

    Delete run_swinT_lscc.sh

--- run_swinT_lscc.sh
+++
@@ -1,8 +0,0 @@
-
-#
-for seed in 3407
-do
-    echo "Running with random_seed=$seed"
-    python HCFMIL_main_lscc.py --abla_type sota --run_mode test --random_seed ${seed} --bag_weight \
-            --bags_len 1042
-done
